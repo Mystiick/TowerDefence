5cc6c0482c57f260870c92d9cb93e3238edfd75d
Jake Congdon <Mystiick@outlook.com>
2020-08-23 18:46:58 -0500

    Missed files

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public static class Extensions
+{
+    public static Vector3 Times(this Vector3 me, Vector3 them)
+    {
+        return new Vector3(
+            me.x * them.x,
+            me.y * them.y,
+            me.z * them.z
+        );
+    }
+
+    public static Vector3 Mod(this Vector3 me, float modAmount)
+    {
+        return new Vector3(
+            me.x % modAmount,
+            me.y % modAmount,
+            me.z % modAmount
+        );
+    }
+}
